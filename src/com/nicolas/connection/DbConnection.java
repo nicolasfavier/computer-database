f53a69fe16c87f5cee4cfef604469b0a1015d065
nicolasfavier <nicolas@desktop-0129.ebi.excilys.com>
2015-02-10 16:02:48 +0100

    connection in instance

--- src/com/nicolas/connection/DbConnection.java
+++ src/com/nicolas/connection/DbConnection.java
@@ -5,28 +5,39 @@ import java.sql.SQLException;
 
 public enum DbConnection {
 	INSTANCE;
-	
+
 	private static final String DB_HOST = "localhost";
 	private static final String DB_PORT = "3306";
 	private static final String DB_NAME = "computer-database-db";
 	private static final String DB_TIME_BEHAVIOR = "?zeroDateTimeBehavior=convertToNull";
 	private static final String DB_USER = "admincdb";
 	private static final String DB_PWD = "qwerty1234";
-	
-	private Connection connection = null;
-	
-	private DbConnection(){}
-	
-	public Connection getConnection(){
-		 Connection connection = null;
-		 try {
-				connection = java.sql.DriverManager.getConnection("jdbc:mysql://"
-						+ DB_HOST + ":" + DB_PORT + "/" + DB_NAME
-						+ DB_TIME_BEHAVIOR, DB_USER, DB_PWD);
-			} catch (SQLException e) {
-				System.out.print("with error");
-				e.printStackTrace();
-			}	
+
+	private DbConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getConnection() {
+		Connection connection = null;
+		try {
+			connection = java.sql.DriverManager.getConnection("jdbc:mysql://"
+					+ DB_HOST + ":" + DB_PORT + "/" + DB_NAME
+					+ DB_TIME_BEHAVIOR, DB_USER, DB_PWD);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return connection;
+	}
+	
+	public void closeConnection(Connection connection) {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
