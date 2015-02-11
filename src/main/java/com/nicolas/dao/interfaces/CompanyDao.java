015328857625202ae7b9b0c16ae6399030affc86
nicolasfavier <nicolas@desktop-0129.ebi.excilys.com>
2015-02-11 11:16:28 +0100

    add dao interface javadoc

--- src/main/java/com/nicolas/dao/interfaces/CompanyDao.java
+++ src/main/java/com/nicolas/dao/interfaces/CompanyDao.java
@@ -5,6 +5,16 @@ import java.util.List;
 import com.nicolas.models.Company;
 
 public interface CompanyDao {
+	/**
+	 * get a company by ID
+	 * @param companyId the ID of the company needed
+	 * @return the company find or null if not found
+	 */
 	public Company getByID(int companyId) ;
+	
+	/**
+	 * get all company present in the database
+	 * @return a list of companies or an empty List if there is no companies
+	 */
 	public List<Company> getAll();
 }
