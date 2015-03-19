177117a0f0aecad01875dbe282583913c5a90b59
nicolasfavier <nicolas@desktop-0129.ebi.excilys.com>
2015-03-19 12:29:50 +0100

    languages

---
+++ computer-database-persistence/src/test/java/com/nicolas/utils/ScriptRunner.java
@@ -0,0 +1,38 @@
+package com.nicolas.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 
+ * execute a script to drop and recreate the database for tests
+ *
+ */
+public class ScriptRunner {
+
+	public static void runScript() {
+		String line;
+		Process p;
+		try {
+			p = Runtime.getRuntime().exec("./script.sh");
+		} catch (Exception err) {
+			throw new RuntimeException(err);
+		}
+		BufferedReader input = new BufferedReader(new InputStreamReader(
+				p.getInputStream()));
+		try {
+			while ((line = input.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				input.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
